d5f45de5cca20e504517f185ac4565b5728093ad
MilenaCLeme <milenaleme4@hotmail.com>
2021-06-07 14:50:31 -0300

    inserindo funçao para adicionar a tarefa em lista e remove no imput

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 let botao = document.getElementById('criar-tarefa');
+let elementoOl = document.getElementById('lista-tarefas')
 criarElementoOl()
+clicarDoBotao()
 
 
 function criarElementoOl() {
   let elementoIdTabela =  document.getElementById('tabela')
   let elementoOl = document.createElement('ol')
   elementoIdTabela.appendChild(elementoOl);
-  elementoOl.id = "lista-tarefas";  
+  elementoOl.id = 'lista-tarefas';  
 }
 
-
-function clicarDoBotaoEnviar(){
-  botao.addEventListener('click', adicionaOElementoETexto);
+function clicarDoBotao(){
+  botao.addEventListener('click', adicionaATarefa)
 }
 
-function adicionaOElementoETexto() {
+function adicionaATarefa() {
+  let textInput = document.getElementById('texto-tarefa').value;
+  let elementoLi = document.createElement('li')
+  let elementoOl = document.getElementById('lista-tarefas')
+  elementoOl.appendChild(elementoLi)
+  elementoLi.innerHTML = textInput
+  removeNoImput()
 }
 
-
+function removeNoImput() {
+  document.getElementById('texto-tarefa').value = ''
+}
