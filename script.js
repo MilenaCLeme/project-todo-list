c8902b266b7a37de15a4678e6bc6ce9d20656b5c
MilenaCLeme <milenaleme4@hotmail.com>
2021-06-07 19:36:05 -0300

    Inserindo funçao para apagar todas as tarefas e tambem as completadas

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-let botao = document.getElementById('criar-tarefa');
-let elementoOl = document.getElementById('lista-tarefas');
-let elementoLi = document.getElementsByTagName('li');
-let ol = document.querySelector('lista-tarefas');
-criarElementoOl()
-clicarDoBotao()
+const elementoIdTabela =  document.getElementById('tabela');
+const botaoEnviar = document.getElementById('criar-tarefa');
+const botaoApagaTudo = document.getElementById('apaga-tudo');
+const botaoApagaFinalizados = document.getElementById('remover-finalizados');
+
 
 function criarElementoOl() {
-  let elementoIdTabela =  document.getElementById('tabela')
   let elementoOl = document.createElement('ol')
   elementoIdTabela.appendChild(elementoOl);
-  elementoOl.id = 'lista-tarefas';  
-}
-
-function clicarDoBotao(){
-  botao.addEventListener('click', adicionaATarefa)
+  elementoOl.id = 'lista-tarefas';   
 }
 
 function adicionaATarefa() {
   let textInput = document.getElementById('texto-tarefa').value;
   let elementoLi = document.createElement('li')
-  let elementoOl = document.getElementById('lista-tarefas')
-  elementoOl.appendChild(elementoLi)
+  let elementoPai = document.getElementById('lista-tarefas');
+  elementoPai.appendChild(elementoLi)
   elementoLi.innerHTML = textInput
   elementoLi.addEventListener('click', adicionaCor)
   elementoLi.addEventListener('dblclick', adicionaUmRiscoATarefa)
@@ -30,6 +24,7 @@ function adicionaATarefa() {
 function removeNoImput() {
   document.getElementById('texto-tarefa').value = ''
 }
+
 
 function adicionaCor(event) {
   let elemento = document.querySelector('.selected');
@@ -42,7 +37,72 @@ function adicionaCor(event) {
 }
 
 function adicionaUmRiscoATarefa(event){
-  console.log('funcionando');
   event.target.classList.toggle('completed');
 }
 
+
+function apagaTudoDaLista() {
+  let elementoPai = document.getElementById('lista-tarefas')
+  elementoPai.remove()
+  criarElementoOl()
+/*
+  let elementoPai = document.getElementById('lista-tarefas');
+  let listaCompleta = document.getElementsByTagName('li');
+  for(let index = 0; index < listaCompleta.length; index += 1) {
+    elementoPai.removeChild(listaCompleta[index])
+    index -= 1;
+  }
+*/
+}
+
+function apagaTarefasFinalizadas() {
+  let listaCompleta = document.getElementsByTagName('li');
+  let elementoPai = document.getElementById('lista-tarefas');
+  for(let index = 0; index < listaCompleta.length; index += 1) {
+    if (listaCompleta[index].classList.contains('completed')) {
+      elementoPai.removeChild(listaCompleta[index])
+      index -= 1;
+    }
+  }
+}
+
+criarElementoOl();
+botaoEnviar.addEventListener('click', adicionaATarefa)
+botaoApagaTudo.addEventListener('click', apagaTudoDaLista)
+botaoApagaFinalizados.addEventListener('click', apagaTarefasFinalizadas)
+
+
+
+
+
+
+/*
+let botao = document.getElementById('criar-tarefa');
+let ol = document.querySelector('lista-tarefas');
+let botaoApagaTudo = document.getElementById('apaga-tudo');
+let botaoApagaFinalizados = document.getElementById('remover-finalizados');
+criarElementoOl()
+clicarDoBotao()
+
+
+function apagaTudoDaLista() {
+  let lista = document.getElementById('lista-tarefas');
+  lista.remove(); 
+}
+
+function apagaTarefasFinalizadas() {
+  let listaCompleta = document.getElementsByTagName('li');
+  let elementoOl = document.getElementById('lista-tarefas');
+  for(let index = 0; index < listaCompleta.length; index += 1) {
+    if (listaCompleta[index].classList.contains('completed')) {
+      elementoOl.removeChild(listaCompleta[index])
+      index -= 1;
+    }
+  }
+
+
+
+/*
+botaoApagaTudo.addEventListener('click', apagaTudoDaLista)
+botaoApagaFinalizados.addEventListener('click', apagaTarefasFinalizadas)
+*/
